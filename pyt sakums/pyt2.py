1f25a0f8d0e4b76d598ebf3d47f705da862c73e5
aP0caypse <therebel360@gmail.com>
2021-09-30 15:08:20 +0300

    Create pyt2.py

---
+++ pyt sakums/pyt2.py
@@ -0,0 +1,41 @@
+import math
+#koeficenta noteiksana taisnes vienadojumam
+def horny_koef(x1,y1,x2,y2):
+     k=(y1-y2)/(x1-x2)
+     b=y1-k*k1
+     koef=[]
+     koef[0]=k
+     koef[1]=b
+     return koef
+def p_koord (x1,y1,x2,y2):
+    dx=x2-x1
+    dy=y2-y1
+    x3=x1+2*dx
+    y3=x1+2*dx
+    P+[]
+    p[0]=x3
+    p[1]=y3
+
+
+xa=float(input("ievadi xa:"))
+ya=float(input("ievadi ya:"))
+xb=float(input("ievadi xb:"))
+yb=float(input("ievadi yb:"))
+xc=float(input("ievadi xc:"))
+yc=float(input("ievadi yc:"))
+
+#parbaude
+#seit vajag
+
+xo=float(input("ievadi xa:"))
+yo=float(input("ievadi xa:"))
+#A_0 xa ya xo yo
+#y=kx+b
+#xa ya xb yb sc yc xo yo
+
+a1=[]
+b1=[]
+c1=[]
+print(p_koord(xa,ya,xo,yo)) #a1=p_koord
+print(p_koord(xb,yb,xo,yo) )#b1=p_koord
+print(p_koord(xb,yb,xo,yo)) #c1=p_koord
